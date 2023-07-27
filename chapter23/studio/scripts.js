831de8c5d6301aca4c2fc7019af99e07429f9a0e
Kara <138933735+KaraAllison@users.noreply.github.com>
2023-07-27 11:48:41 -0600

    Chapter25 complete

--- chapter23/studio/scripts.js
+++ chapter23/studio/scripts.js
@@ -75,22 +75,6 @@ function init () {
         shuttleHeight.innerHTML = String(Number(shuttleHeight.innerHTML) + 10000);
     }
     })
-    // // Put your code for the exercises here.
-    // button.addEventListener("click", function(){
-    //     paragraph.innerHTML = "Houston, we have liftoff!";
-    // })
-    // missionAbort.onmouseover = function(){
-    //     this.style.backgroundColor = "red";
-    // }
-    // missionAbort.onmouseout = function(){
-    //     this.style.backgroundColor = "";
-    // }
-    // missionAbort.addEventListener("click", function(){
-    //     let abort = window.confirm("Are you sure you want to abort the mission?");
-    //     if (abort) {
-    //         paragraph.innerHTML = "Mission aborted! Space shuttle returning home.";
-    //     }
-    // })
     
 }
 
